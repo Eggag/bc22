9d1f25c5b19da896256d1224bda9f94dc89a538c
Egor Gagushin <eggag03@gmail.com>
2022-01-15 21:49:11 -0500

    clean up orzbot

--- src/orzbot/Archon.java
+++ src/orzbot/Archon.java
@@ -7,58 +7,21 @@ import java.util.Map;
 
 public class Archon extends RobotPlayer {
 
-    static int id = 0;
+    static int id = -1;
 
-    static void build(int ind) throws GameActionException {
-        RobotType rt = RobotType.MINER;
-        if(ind == 1) rt = RobotType.SOLDIER;
-        for (int i = 0; i < 8; i++) {
-            Direction dir = directions[i];
-            if (rc.canBuildRobot(rt, dir)) {
-                rc.buildRobot(rt, dir);
-            }
-        }
+    static void decideBuild() throws GameActionException{
+        //don't forget to update income
     }
-
-    static void decideBuild(int a, int b) throws GameActionException{
-        RobotInfo[] en = rc.senseNearbyRobots(1000, rc.getTeam().opponent());
-        for(RobotInfo uwu : en){
-            if(rc.getType() == RobotType.SOLDIER || rc.getType() == RobotType.SAGE || rc.getType() == RobotType.WATCHTOWER){
-                build(1);
-                return;
-            }
-        }
-        if(a < 20){
-            build(0);
-        }
-        else if(a < 30){
-            if(b < a / 3) build(1);
-            else build(0);
-        }
-        else if(a < 40){
-            if(b < a) build(1);
-            else build(0);
-        }
-        else if(a < 45){
-            if(b / 2 < a) build(1);
-            else build(0);
-        }
-        else{
-            if(b / 4 < a) build(1);
-            else build(0);
-        }
-    }
-
 
     static void runArchon() throws GameActionException {
         if(turnCount == 1){
-            int cr = rc.readSharedArray(59);
+            int cr = rc.readSharedArray(NUM_ARCHONS_IND);
             id = cr;
-            rc.writeSharedArray(59, cr + 1);
-            for(int i = 0; i < 60; i++){
+            rc.writeSharedArray(NUM_ARCHONS_IND, cr + 1);
+            for(int i = 0; i < MAX_MSG; i++){
                 int num = rc.readSharedArray(i);
-                if (num == 0) {
-                    num = 3;
+                if (num == 0){
+                    num = OUR_ARCHON_CODE;
                     num |= (rc.getLocation().x << 9);
                     num |= (rc.getLocation().y << 3);
                     rc.writeSharedArray(i, num);
@@ -66,23 +29,12 @@ public class Archon extends RobotPlayer {
                 }
             }
         }
-        tryReportR();
-        int numMiner = rc.readSharedArray(63);
-        if(rc.getRoundNum() % 2 == 0){
-            numMiner = rc.readSharedArray(62);
-            rc.writeSharedArray(63, 0);
+        int lst = rc.readSharedArray(INCOME_IND);
+        int inc = rc.getTeamLeadAmount(rc.getTeam()) - lst;
+        rc.setIndicatorString("INCOME IS " + inc);
+        if(rc.getRoundNum() == 1) inc = 0;
+        if(rc.getRoundNum() % rc.readSharedArray(NUM_ARCHONS_IND) == id){
+            decideBuild();
         }
-        else{
-           rc.writeSharedArray(62, 0);
-        }
-        int numSoldier = rc.readSharedArray(61);
-        if(rc.getRoundNum() % 2 == 0){
-            numSoldier = rc.readSharedArray(60);
-            rc.writeSharedArray(61, 0);
-        }
-        else{
-            rc.writeSharedArray(60, 0);
-        }
-        if(rc.getRoundNum() % rc.readSharedArray(59) == id) decideBuild(numMiner, numSoldier);
     }
 }
--- src/orzbot/RobotPlayer.java
+++ src/orzbot/RobotPlayer.java
@@ -9,6 +9,14 @@ public strictfp class RobotPlayer {
     static int turnCount = 0;
     static final Random rng = new Random();
 
+    static int NUM_SOLDIERS_IND = 63;
+    static int NUM_MINERS_IND = 62;
+    static int NUM_ARCHONS_IND = 61;
+    static int INCOME_IND = 60;
+    static int MAX_MSG = 60;
+
+    static int OUR_ARCHON_CODE = 1;
+
     static final Direction[] directions = {
         Direction.NORTH,
         Direction.NORTHEAST,
@@ -22,12 +30,10 @@ public strictfp class RobotPlayer {
 
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
-        // You can also use indicators to save debug notes in replays.
-        rc.setIndicatorString("Hello world!");
         RobotPlayer.rc = rc;
         while (true) {
             turnCount += 1;
-            try {
+            try{
                 switch (rc.getType()) {
                     case ARCHON:     Archon.runArchon();  break;
                     case MINER:      Miner.runMiner();   break;
@@ -51,61 +57,4 @@ public strictfp class RobotPlayer {
             }
         }
     }
-
-
-    static void tryReportR() throws GameActionException {
-        //try to delete stuff that isnt there
-        int l = 0, r = 32;
-        if(turnCount % 2 == 1){
-            l = 32;
-            r = 59;
-        }
-        for(int t = 1; t <= 2; t++) {
-            for(int i = l; i < r; i++) {
-                int num = rc.readSharedArray(i);
-                if ((num & 0b111) == t) {
-                    int x = ((num >> 9) & 0b111111), y = (num >> 3) & 0b111111;
-                    MapLocation loc = new MapLocation(x, y);
-                    if(t == 1){
-                        if(rc.canSenseLocation(loc) && rc.senseGold(loc) == 0) rc.writeSharedArray(i, 0);
-                    }
-                    else{
-                        if(rc.canSenseLocation(loc) && rc.senseLead(loc) == 0) rc.writeSharedArray(i, 0);
-                    }
-                }
-            }
-        }
-        //we try to report resources we can see
-        for(int t = 1; t <= 2; t++) {
-            MapLocation[] cur = rc.senseNearbyLocationsWithGold(1000);
-            if(t == 2){
-                cur = rc.senseNearbyLocationsWithLead(1000);
-            }
-            for (MapLocation loc : cur) {
-                int mnDist = 1000000;
-                for (int i = 0; i < 59; i++) {
-                    int num = rc.readSharedArray(i);
-                    if ((num & 0b111) == t) {
-                        int x = ((num >> 9) & 0b111111), y = (num >> 3) & 0b111111;
-                        int curDist = loc.distanceSquaredTo(new MapLocation(x, y));
-                        if (curDist < mnDist) mnDist = curDist;
-                    }
-                    if(mnDist <= 20) break;
-                }
-                if (mnDist > 20) {
-                    for (int i = 0; i < 59; i++) {
-                        int num = rc.readSharedArray(i);
-                        if (num == 0) {
-                            num = t;
-                            num |= (loc.x << 9);
-                            num |= (loc.y << 3);
-                            rc.writeSharedArray(i, num);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
 }
