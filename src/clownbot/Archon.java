7e6a8662a3dc333d949b7f8441b726527ecdefe1
Alex Fan <codetiger927@gmail.com>
2022-01-21 22:02:50 -0500

    clown miner

--- src/clownbot/Archon.java
+++ src/clownbot/Archon.java
@@ -4,9 +4,13 @@ import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotType;
 
+import java.awt.*;
+
 public class Archon extends RobotPlayer {
 
     static void runArchon() throws GameActionException {
-
+        if(turnCount == 1) {
+            rc.buildRobot(RobotType.MINER,directions[0]);
+        }
     }
 }
--- src/clownbot/Miner.java
+++ src/clownbot/Miner.java
@@ -4,9 +4,13 @@ import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 
 public class Miner extends RobotPlayer {
-
+    static MapLocation target;
     static void runMiner() throws GameActionException{
-
+        if(turnCount == 1) {
+            target = new MapLocation(rc.getMapWidth() - 1 - rc.getLocation().x,rc.getMapHeight() - 1 - rc.getLocation().y);
+        }else{
+            Navigation.go(target);
+        }
     }
 
 }
