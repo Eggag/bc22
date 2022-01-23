b9942deb7a14d6ffdadd8900c45d2efd7a4b4ebf
Egor Gagushin <eggag03@gmail.com>
2022-01-23 01:27:10 -0500

    add miner code back

--- src/clownbot/Miner.java
+++ src/clownbot/Miner.java
@@ -1,16 +1,145 @@
 package clownbot;
 
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
+import battlecode.common.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Miner extends RobotPlayer {
-    static MapLocation target;
-    static void runMiner() throws GameActionException{
-        if(turnCount == 1) {
-            target = new MapLocation(rc.getMapWidth() - 1 - rc.getLocation().x,rc.getMapHeight() - 1 - rc.getLocation().y);
-        }else{
-            Navigation.go(target);
+
+    static boolean scouting = false;
+    static MapLocation scoutGoal = null;
+    static RobotInfo[] enemies;
+
+    static void tryMine() throws GameActionException {
+        MapLocation me = rc.getLocation();
+        int av = 0;
+        boolean far = true;
+        for (RobotInfo en : enemies) {
+            if (en.getType() == RobotType.ARCHON) far = false;
         }
+        for (int t = 0; t < 2; t++) {
+            MapLocation[] cur;
+            if (t == 0) cur = rc.senseNearbyLocationsWithGold(1000);
+            else cur = rc.senseNearbyLocationsWithLead(1000);
+            for (MapLocation nw : cur) {
+                if (!rc.isActionReady()) break;
+                if (rc.getLocation().isAdjacentTo(nw)) {
+                    if (t == 0) {
+                        while (rc.canMineGold(nw)) {
+                            rc.mineGold(nw);
+                        }
+                    } else {
+                        while (rc.canMineLead(nw)) {
+                            if (rc.senseLead(nw) == 1 && far) break;
+                            rc.mineLead(nw);
+                        }
+                    }
+                    scouting = false;
+                }
+            }
+        }
+        if (!rc.isMovementReady()) return;
+        for (int t = 0; t < 2; t++) {
+            MapLocation[] cr;
+            if (t == 0) cr = rc.senseNearbyLocationsWithGold(1000);
+            else cr = rc.senseNearbyLocationsWithLead(1000);
+            int d = 1000000;
+            MapLocation bst = null;
+            for (MapLocation loc : cr) {
+                int di = rc.getLocation().distanceSquaredTo(loc);
+                int amt = rc.senseGold(loc);
+                if (t == 1) amt = rc.senseLead(loc);
+                if (t == 1 && amt == 1) continue;
+                if ((di - amt / 10) < d) {
+                    d = di - amt / 10;
+                    bst = loc;
+                }
+            }
+            if (bst != null) {
+                Navigation.go(bst);
+                break;
+            }
+        }
+    }
+
+    static void tryScout() throws GameActionException {
+        if (!rc.isMovementReady()) return;
+        if (scouting && rc.getLocation().distanceSquaredTo(scoutGoal) > 20) Navigation.go(scoutGoal);
+        else {
+            double rand = Math.random();
+            if (rand < 0.7) {
+                scoutGoal = new MapLocation((Math.abs(rng.nextInt())) % rc.getMapWidth(), (Math.abs(rng.nextInt())) % rc.getMapHeight());
+            } else {
+                if (rc.getRoundNum() % 4 == 0) {
+                    scoutGoal = new MapLocation(Math.min(rc.getMapWidth() - 1, rc.getLocation().x + 10), Math.min(rc.getMapHeight() - 1, rc.getLocation().y - 10));
+                }
+                if (rc.getRoundNum() % 4 == 1) {
+                    scoutGoal = new MapLocation(Math.min(rc.getMapWidth() - 1, rc.getLocation().x + 10), Math.max(0, rc.getLocation().y - 10));
+                }
+                if (rc.getRoundNum() % 4 == 2) {
+                    scoutGoal = new MapLocation(Math.max(0, rc.getLocation().x - 10), Math.min(rc.getMapHeight() - 1, rc.getLocation().y + 10));
+                } else {
+                    scoutGoal = new MapLocation(Math.max(0, rc.getLocation().x - 10), Math.max(0, rc.getLocation().y - 10));
+                }
+            }
+            scouting = true;
+            Navigation.go(scoutGoal);
+        }
+    }
+
+    static void addDanger() throws GameActionException {
+        int message = rc.readSharedArray(AGGRO_IND);
+        for (RobotInfo uwu : enemies) {
+            if (uwu.type == RobotType.SOLDIER) {
+                message++;
+            }
+            if (uwu.type == RobotType.MINER) {
+                message++;
+            }
+        }
+        rc.writeSharedArray(AGGRO_IND, message);
+    }
+
+
+    static void tryRun() throws GameActionException {
+        if (!rc.isMovementReady()) return;
+        enemies = rc.senseNearbyRobots(1000, rc.getTeam().opponent());
+        Hotspot.addEnemies(enemies);
+        int mn = 10000000;
+        MapLocation danger = null;
+        for (RobotInfo uwu : enemies) {
+            if (uwu.type == RobotType.SOLDIER || uwu.type == RobotType.WATCHTOWER || uwu.type == RobotType.SAGE) {
+                int d = rc.getLocation().distanceSquaredTo(uwu.location);
+                if (d < mn) {
+                    mn = d;
+                    danger = uwu.location;
+                }
+            }
+        }
+        if (danger == null) return;
+        MapLocation bst = null;
+        int sc = 0;
+        for (int i = -5; i <= 5; i++) {
+            for (int j = -5; j <= 5; j++) {
+                MapLocation pos = new MapLocation(rc.getLocation().x + i, rc.getLocation().y + j);
+                if (danger.distanceSquaredTo(pos) > sc) {
+                    sc = danger.distanceSquaredTo(pos);
+                    bst = pos;
+                }
+            }
+        }
+        if (bst != null) Navigation.go(bst);
+    }
+
+    static void runMiner() throws GameActionException {
+        updateAlive(NUM_MINERS_IND);
+        tryRun();
+        addDanger();
+        tryMine();
+        tryScout();
+        addDanger();
     }
 
 }
--- src/clownbot/RobotPlayer.java
+++ src/clownbot/RobotPlayer.java
@@ -6,6 +6,11 @@ import java.util.Random;
 public strictfp class RobotPlayer {
     static int turnCount = 0;
     static RobotController rc;
+    static final Random rng = new Random();
+
+    static int NUM_MINERS_IND = 63;
+    static int NUM_SOLDIER_IND = 62;
+    static int AGGRO_IND = 61;
 
     static final Direction[] directions = {
             Direction.NORTH,
@@ -52,6 +57,11 @@ public strictfp class RobotPlayer {
     static boolean attackingUnit(RobotInfo rb) throws GameActionException{
         if(rb.getType() == RobotType.SOLDIER || rb.getType() == RobotType.WATCHTOWER || rb.getType() == RobotType.SAGE) return true;
         return false;
+    }
+
+    static void updateAlive(int index) throws GameActionException{
+        int nm = rc.readSharedArray(index);
+        rc.writeSharedArray(index, nm + 1);
     }
 
 }
